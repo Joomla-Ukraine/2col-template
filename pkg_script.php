49b9b3c747410d304d0db358ca937a18fca3ced1
Denys <denys@joomla-ua.org>
2021-01-05 05:52:56 +0200

    Init

---
+++ pkg_script.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * JU 2col
+ *
+ * @package          Joomla.Site
+ * @subpackage       JU 2col
+ *
+ * @author           Denys Nosov, denys@joomla-ua.org
+ * @copyright        2019-2021 (C) Joomla! Ukraine, http://joomla-ua.org. All rights reserved.
+ * @license          GNU General Public License version 2 or later; see _LICENSE.php
+ */
+
+defined('_JEXEC') or die;
+
+class pkg_ju_2colInstallerScript extends JCckInstallerScriptApp
+{
+	protected $template_placeholder = 'ju_2col';
+}
